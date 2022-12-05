23b8961bd36be5eb320a4dc878ffdb25be840cf4
TienDong <dongpt410@gmail.com>
2022-12-05 14:17:58 +0300

    first commit by Dong

---
+++ footer.php
@@ -0,0 +1,49 @@
+<link rel="stylesheet" type="text/css" href="css/footer.css">
+<footer>
+<div class="container">
+<div class="follow_us">
+    <label>Follow Us</label>
+    <ul>
+        <li><a href="https://vk.com/mirea_official">VKontakte MIREA</a></li>
+        <li><a href="https://www.mirea.ru/">WEBSITE MIREA</a></li>
+        <li><a href="https://vk.com/id602943301">VKontakte of me</a></li>
+    
+    </ul>
+    </div>
+    <div class="contact" id="contactus">
+        <label>Contact Us</label>
+        <ul>
+            <p>Russia, Moscow</p><p>11-ya Parkovaya, 36 105077</p>
+            <p>Tel : +79153326578</p>
+            <p> Email: dongpt410@gmail.com</p>
+            
+        </ul>
+    </div>
+        <div class="links">
+            <label>Important Links</label>
+            <ul>    
+            <li><a href="index.php#aboutus" target="_blank">About Us</a></li>
+            <li><a href="https://bank.codes/swift-code/russia/">Bank IFSC Code</a></li>
+            <li><a href="gst.php">Goods & Services Tax</a></li>
+            <li><a href="emi_calculator.php">Calculator</a></li>
+            </ul><br>
+    </div>
+     <div class="About_GST">
+            <label>About GST</label>
+            <ul>    
+            <li><a href="gst.php">GST Council Structure</a></li>
+            <li><a href="gst.php">GST History</a></li>
+            </ul><br>
+    </div>
+    
+    </div>
+    <div class="copyright">
+        <span>Copyright &copy; 2022 Student Bank. All rights reserved.</span>
+    </div>
+    
+    <div class="bestview">
+    <span>Web design by PhuongTienDong</span>
+    
+    </div>
+
+</footer>
---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+<title>Student Bank</title>
+<head>
+   <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/js" href="bootstrap.min.js">
+   <link rel="shortcut icon" href="chase _title.jpg">
+    </head>
+ 
+<body>
+<?php include'header.php'?>
+<div class="index_container">
+    <div class="slider">
+    <div class="slideimg">
+        <img src="img/slide-1.jpg">
+        <img src="img/slide%20(2).jpg">
+        <img src="img/slide%20(3).jpg">
+        <img src="img/slide%20(4).jpg">
+        <img src="img/slide%20(5).jpg">
+        </div>
+    </div><br>
+    <div class="newsroom">
+        
+        <marquee class="marquee_news" scrolldelay="20"><p class="newsfeed"><span>Your bank may charge you for closing a bank account. 
+        Bank also charges you when you close your account within a particular time period.</span>
+                <span>SBI cuts deposit rates; PPF to fetch lower interest rate. </span>
+                <span>No, it is not mandatory to link your bank account with Visa card</span></p></marquee>
+    </div><br><br>
+   
+
+    
+<div class="news_events">
+    <h4>News | Updates | Events</h4><br>
+        <ul>
+            <a href="https://www.banki.ru/news/lenta/?id=10976280"><li>The Central Bank fined VTB Bank</li></a><br>
+            <a href="https://www.banki.ru/news/lenta/?id=10976276"><li>ACRA assigned Unistream Bank a BB-(RU) credit rating</li></a><br>
+            <a href="https://www.banki.ru/news/lenta/?id=10976217"><li>Central Bank support measures expire: how they affected banks and what will happen next</li></a>
+            <br>
+        </ul>
+    </div>
+    
+
+    <div class="online_services">
+        <h4>Online Services</h4>
+        <ul>
+            <a href="customer_reg_form.php"><li>Open Account</li></a>
+            <a href="debit_card_form.php"><li>Apply Debit Card</li></a><br>
+            <a href="" id="ebanking" ><li><div class="ebanking">Internet Banking
+                <div class = "ebanking_options">
+                <ul>
+                    <a href="customer_login.php"><li>Login </li></a>
+                    <a href="ebanking_reg_form.php"><li>Register</li></a>
+                </ul>
+            </div>
+        </div>
+    </li></a>
+            <a href="fund_transfer.php"><li>Fund Transfer</li></a><br>
+            <a href="https://yandex.ru/maps/213/moscow/house/11_ya_parkovaya_ulitsa_36/Z04YfgdlT00HQFtvfXRxcH1hZQ==/?from=tabbar&ll=37.805680%2C55.801121&z=17.21"><li>Branch Locator</li></a>
+            <a href="https://bank.codes/swift-code/russia/"><li>Find IFSC Code</li></a>
+        </ul>
+   
+</div>
+
+        <div id="aboutus" class="about"><span>About Us</span><br><br>
+        <p>Student Bank of Russia (SBR) is one of the banks which were nationalized on Octorber 22, 2022. Presently the Bank is having a Three-tier organisational set-up consisting of the Head Office,  Regional Offices and the Branches.
+
+            The Bank expanded its branch network in a big way and actively participated in the developmental activities, particularly in the rural and semi-urban areas in conformity with the objectives of nationalisation. In recognition of the role played by the Bank, it was designated as Lead Bank in several districts and at present it is the Lead Bank  in Russia</p></div>
+    
+    <div class="disclaimer">
+    <spasn>Disclaimer !!</spasn><br><br>
+        <p>Our bank does not ask for the details of your account/PIN/password. Therefore any one pretending to be asking you for information from the bank/technical team may be fraudulent entities, so please beware.</p>
+        <p>You should know how to operate net transactions and if you are not familiar you may refrain from doing so. You may seek bank's guidance in this regard. Bank is not responsible for online transactions going wrong.</p>
+        <p>We shall also not be responsible for wrong transactions and wanton disclosure of details by you. Viewing option and transaction option on the net are different. You may exercise your option diligently.</p>
+    </div>
+
+
+    </div>
+    
+<?php include 'footer.php';?>
+</body>
+
+</html>
